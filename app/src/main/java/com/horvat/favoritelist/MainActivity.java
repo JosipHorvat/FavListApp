3552b6506d520e0b11b635ec12007bb7bb62e8b4
JosipHorvat <josiph988@gmail.com>
2020-12-11 20:46:04 +0100

    FloatingActionButton and AlertDialog with EditText

--- app/src/main/java/com/horvat/favoritelist/MainActivity.java
+++ app/src/main/java/com/horvat/favoritelist/MainActivity.java
@@ -1,19 +1,24 @@
 package com.horvat.favoritelist;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.InputType;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +35,15 @@ public class MainActivity extends AppCompatActivity {
         categoryRecyclerView = findViewById(R.id.category_recyclerview);
         categoryRecyclerView.setAdapter(new CategoryRecyclerAdapter());
         categoryRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "FAB tapped", Toast.LENGTH_SHORT).show();
+                displayCreateCategoryDialog();
+            }
+        });
 
     }
 
@@ -53,5 +67,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void displayCreateCategoryDialog(){
+
+        String alertTitle = "Enter the name of the category";
+        String positiveButtonTitle = "Create";
+
+        //There is no anonymus class and I can set only "this" as Context
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        EditText categoryEditText = new EditText(this);
+        categoryEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        alertBuilder.setTitle(alertTitle);
+        alertBuilder.setView(categoryEditText);
+
+        // onClick listener for positive Button     NEW DIALOGINTERFACE.ONCLICK LISTENER IS ANONYMUS CLASS
+        alertBuilder.setPositiveButton(positiveButtonTitle, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+
+        alertBuilder.create().show();
+
     }
 }
